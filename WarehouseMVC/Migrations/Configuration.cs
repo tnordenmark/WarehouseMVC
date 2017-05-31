04780b80f25026d2500a41f9d1daf544a5e3d0aa
Thomas Nordenmark <t.nordenmark@gmail.com>
2017-05-31 20:20:55 +0100

    Initial commit.

--- WarehouseMVC/Migrations/Configuration.cs
+++ WarehouseMVC/Migrations/Configuration.cs
@@ -4,22 +4,28 @@ namespace WarehouseMVC.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using WarehouseMVC.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WarehouseMVC.DataAccess.StoreContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(WarehouseMVC.DataAccess.StoreContext context)
         {
-            context.Items.AddOrUpdate(
-                i => i.ArticleNumber,
-                new StockItem { ArticleNumber = 1000, Name = "Stor häst", Price = 30000, ShelfPosition = "A1", Quantity = 10, Description = "Mycket stor häst" },
-                new StockItem { ArticleNumber = 2000, Name = "Liten häst", Price = 20000, ShelfPosition = "B1", Quantity = 20, Description = "Mycket liten häst" }
-                );
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
         }
     }
 }
