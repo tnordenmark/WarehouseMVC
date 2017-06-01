6821864bdb4f91c567969accc79f03d48fb5d7db
tnordenmark <t.nordenmark@gmail.com>
2017-06-01 09:04:55 +0200

    Fixed Database seed function.

--- WarehouseMVC/Migrations/Configuration.cs
+++ WarehouseMVC/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace WarehouseMVC.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WarehouseMVC.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WarehouseMVC.DataAccess.StoreContext>
     {
@@ -26,6 +27,98 @@ namespace WarehouseMVC.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            context.Items.AddOrUpdate(
+                new StockItem
+                {
+                    ArticleNumber = "H2365",
+                    Name = "Large horse",
+                    Category = ProductCategory.Horses,
+                    ShelfPosition = "H1",
+                    Quantity = 10,
+                    Price = 30000,
+                    Description = "Very large horse. For maximum stamina and speed."
+                },
+                new StockItem
+                {
+                    ArticleNumber = "H1623",
+                    Name = "Medium horse",
+                    Category = ProductCategory.Horses,
+                    ShelfPosition = "H2",
+                    Quantity = 15,
+                    Price = 25000,
+                    Description = "Medium sized horse. Just the right size."
+                },
+                new StockItem
+                {
+                    ArticleNumber = "H7645",
+                    Name = "Small horse",
+                    Category = ProductCategory.Horses,
+                    ShelfPosition = "H3",
+                    Quantity = 20,
+                    Price = 20000,
+                    Description = "Very tiny horse. For the penny wise equestrian."
+                },
+                new StockItem
+                {
+                    ArticleNumber = "A5687",
+                    Name = "Keyring",
+                    Category = ProductCategory.Accessories,
+                    ShelfPosition = "A7",
+                    Quantity = 200,
+                    Price = 39,
+                    Description = "Extremely beautiful keyring in the shape of a horse."
+                },
+                new StockItem
+                {
+                    ArticleNumber = "C6667",
+                    Name = "Riding helmet",
+                    Category = ProductCategory.Clothing,
+                    ShelfPosition = "C1",
+                    Quantity = 40,
+                    Price = 599,
+                    Description = "Proper head protection is the key for any serious equestrian."
+                },
+                new StockItem
+                {
+                    ArticleNumber = "F1112",
+                    Name = "Oats",
+                    Category = ProductCategory.Food,
+                    ShelfPosition = "F3",
+                    Quantity = 150,
+                    Price = 199,
+                    Description = "Premium oats for any hungry horse."
+                },
+                new StockItem
+                {
+                    ArticleNumber = "F1354",
+                    Name = "Carrots",
+                    Category = ProductCategory.Food,
+                    ShelfPosition = "F2",
+                    Quantity = 100,
+                    Price = 19.90m,
+                    Description = "Carrots are tasty."
+                },
+                new StockItem
+                {
+                    ArticleNumber = "C8865",
+                    Name = "Riding boots",
+                    Category = ProductCategory.Clothing,
+                    ShelfPosition = "C7",
+                    Quantity = 50,
+                    Price = 1299.90m,
+                    Description = "Because leather is the best!"
+                },
+                new StockItem
+                {
+                    ArticleNumber = "A4587",
+                    Name = "Bathtub",
+                    Category = ProductCategory.Accessories,
+                    ShelfPosition = "A3",
+                    Quantity = 5,
+                    Price = 4990,
+                    Description = "Horse sized bathtub well suited for teaching horses to swim."
+                }
+            );
         }
     }
 }
